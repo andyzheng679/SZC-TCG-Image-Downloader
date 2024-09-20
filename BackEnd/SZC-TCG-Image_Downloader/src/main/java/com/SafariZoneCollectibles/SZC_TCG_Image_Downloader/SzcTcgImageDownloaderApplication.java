dff33cd34387f692bc3c06488c8e43c0648d153a
andyzheng679 <andyzheng679@gmail.com>
2024-09-20 02:10:09 -0400

    added backend

---
+++ BackEnd/SZC-TCG-Image_Downloader/src/main/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/SzcTcgImageDownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.SafariZoneCollectibles.SZC_TCG_Image_Downloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzcTcgImageDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzcTcgImageDownloaderApplication.class, args);
+	}
+
+}
---
+++ BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/SzcTcgImageDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SafariZoneCollectibles.SZC_TCG_Image_Downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzcTcgImageDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
