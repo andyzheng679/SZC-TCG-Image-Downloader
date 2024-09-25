0743098c5a253da3c71365786546d9957554528c
andyzheng679 <andyzheng679@gmail.com>
2024-09-25 01:53:28 -0400

    switch statement to find correct endpoint for each tcg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,27 @@ import TCGDropDown from './components/TCGDropDown';
 function App() {
 
   const [selectedTcg, setSelectedTcg] = useState("");
+  const [apiEndPoint, setApiEndPoint] = useState("");
 
   const handleTcgChange = (event) => {
     setSelectedTcg(event.target.value);
-  }
+  };
 
+  useEffect(() => {
+    switch(selectedTcg){
+      case "pokemon":
+        setApiEndPoint("/pokemon/set");
+        break;
+      case "testing":
+        setApiEndPoint("/testing/set");
+        break;
+      case "testing2":
+        setApiEndPoint("/testing2/set");
+        break;
+      default:
+        setApiEndPoint("");
+    }
+  }, [selectedTcg]);
 
 
 
