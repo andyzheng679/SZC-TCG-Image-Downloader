13c17c68885910140b2c32fe88c81278f57541c4
andyzheng679 <andyzheng679@gmail.com>
2024-10-01 22:29:02 -0400

    changed set url to order by set num

--- BackEnd/SZC-TCG-Image_Downloader/src/main/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/controller/LorcanaService.java
+++ BackEnd/SZC-TCG-Image_Downloader/src/main/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/controller/LorcanaService.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class LorcanaService {
 
     private RestTemplate restTemplate;
 
     private ObjectMapper objectMapper;
 
-    String allSetsURL = "https://api.lorcana-api.com/sets/fetch";
+    String allSetsURL = "https://api.lorcana-api.com/sets/fetch?orderby=Set_Num";
 
     @Autowired
     public  LorcanaService(RestTemplate restTemplate, ObjectMapper objectMapper){
@@ -18,7 +21,11 @@ public class LorcanaService {
         this.objectMapper = objectMapper;
     }
 
-    
+    public Map<String, Integer> allSets(String getLorcanaSets){
+        Map<String, Integer> mapSetToSetNum = new LinkedHashMap<>();
+
+        return mapSetToSetNum;
+    }
 
     private String getLorcanaSets(){
         String url = allSetsURL;
