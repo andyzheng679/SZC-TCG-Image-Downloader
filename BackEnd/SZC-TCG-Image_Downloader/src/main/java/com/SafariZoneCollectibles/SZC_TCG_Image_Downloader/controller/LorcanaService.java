60a18a2f0cc55c34099e5dc965960077af89c423
andyzheng679 <andyzheng679@gmail.com>
2024-10-01 22:20:29 -0400

    getting all lorcana sets as a string

--- BackEnd/SZC-TCG-Image_Downloader/src/main/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/controller/LorcanaService.java
+++ BackEnd/SZC-TCG-Image_Downloader/src/main/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/controller/LorcanaService.java
@@ -1,4 +1,29 @@
 package com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
 public class LorcanaService {
+
+    private RestTemplate restTemplate;
+
+    private ObjectMapper objectMapper;
+
+    String allSetsURL = "https://api.lorcana-api.com/sets/fetch";
+
+    @Autowired
+    public  LorcanaService(RestTemplate restTemplate, ObjectMapper objectMapper){
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    
+
+    private String getLorcanaSets(){
+        String url = allSetsURL;
+        return(restTemplate.getForObject(url, String.class));
+    }
+
+
 }
