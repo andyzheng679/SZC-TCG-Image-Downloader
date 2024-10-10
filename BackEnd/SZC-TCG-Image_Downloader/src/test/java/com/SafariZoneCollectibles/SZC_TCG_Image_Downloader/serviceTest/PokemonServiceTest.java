14173509923c5f419209236fc8e30322ebe0a0eb
andyzheng679 <andyzheng679@gmail.com>
2024-10-10 18:07:18 -0400

    mockito pokemon service allSets

--- BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/serviceTest/PokemonServiceTest.java
+++ BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/serviceTest/PokemonServiceTest.java
@@ -1,6 +1,7 @@
 package com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.serviceTest;
 
 import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.service.PokemonService;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,10 +10,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Iterator;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,29 +29,30 @@ public class PokemonServiceTest {
     @InjectMocks
     private PokemonService pokemonService;
 
+    private String json = "{\n" +
+            "  \"data\": [\n" +
+            "    {\n" +
+            "      \"id\": \"base2\",\n" +
+            "      \"name\": \"Jungle\",\n" +
+            "      \"series\": \"Base\",\n" +
+            "      \"printedTotal\": 64,\n" +
+            "      \"total\": 64,\n" +
+            "      \"legalities\": {\n" +
+            "        \"unlimited\": \"Legal\"\n" +
+            "      },\n" +
+            "      \"ptcgoCode\": \"JU\",\n" +
+            "      \"releaseDate\": \"1999/06/16\",\n" +
+            "      \"updatedAt\": \"2020/08/14 09:35:00\",\n" +
+            "      \"images\": {\n" +
+            "        \"symbol\": \"https://images.pokemontcg.io/base2/symbol.png\",\n" +
+            "        \"logo\": \"https://images.pokemontcg.io/base2/logo.png\"\n" +
+            "      }\n" +
+            "    }\n" +
+            "]\n" +
+            "}";
+
     @Test
     void testGetPokemonSets(){
-        String json = "{\n" +
-                "  \"data\": [\n" +
-                "    {\n" +
-                "      \"id\": \"base2\",\n" +
-                "      \"name\": \"Jungle\",\n" +
-                "      \"series\": \"Base\",\n" +
-                "      \"printedTotal\": 64,\n" +
-                "      \"total\": 64,\n" +
-                "      \"legalities\": {\n" +
-                "        \"unlimited\": \"Legal\"\n" +
-                "      },\n" +
-                "      \"ptcgoCode\": \"JU\",\n" +
-                "      \"releaseDate\": \"1999/06/16\",\n" +
-                "      \"updatedAt\": \"2020/08/14 09:35:00\",\n" +
-                "      \"images\": {\n" +
-                "        \"symbol\": \"https://images.pokemontcg.io/base2/symbol.png\",\n" +
-                "        \"logo\": \"https://images.pokemontcg.io/base2/logo.png\"\n" +
-                "      }\n" +
-                "    }\n" +
-                "]\n" +
-                "}";
 
         when(restTemplate.getForObject(anyString(), eq(String.class))).thenReturn(json);
 
@@ -55,5 +60,36 @@ public class PokemonServiceTest {
 
         assertNotNull(result);
         assertEquals(json, result);
+    }
+
+    @Test
+    void testAllSets() throws Exception{
+        JsonNode mockRootNode = mock(JsonNode.class);
+        JsonNode mockDataNode = mock(JsonNode.class);
+        JsonNode mockSetDataNode = mock(JsonNode.class);
+
+        when(objectMapper.readTree(anyString())).thenReturn(mockRootNode);
+        when(mockRootNode.get("data")).thenReturn(mockDataNode);
+
+        Iterator<JsonNode> mockArrayData = mock(Iterator.class);
+        when(mockDataNode.elements()).thenReturn(mockArrayData);
+        when(mockArrayData.hasNext()).thenReturn(true, false);
+        when(mockArrayData.next()).thenReturn(mockSetDataNode);
+
+        when(mockSetDataNode.get("name")).thenReturn(mock(JsonNode.class));
+        when(mockSetDataNode.get("id")).thenReturn(mock(JsonNode.class));
+        when(mockSetDataNode.get("name").asText()).thenReturn("Jungle");
+        when(mockSetDataNode.get("id").asText()).thenReturn("base2");
+
+        Map<String, String> result = pokemonService.allSets(json);
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("base2", result.get("Jungle"));
+
+
+
+
+
     }
 }
