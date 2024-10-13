3bfb9d703d923055b605b5cfb6df869b70a00378
andyzheng679 <andyzheng679@gmail.com>
2024-10-12 22:24:39 -0400

    mockito lorcana service getAllCards test

--- BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/serviceTest/LorcanaServiceTest.java
+++ BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/serviceTest/LorcanaServiceTest.java
@@ -34,7 +34,35 @@ public class LorcanaServiceTest {
             "    \"Cards\": 204,\n" +
             "    \"Name\": \"The First Chapter\",\n" +
             "    \"Set_ID\": \"TFC\"\n" +
-            "  }";
+            "  }]";
+
+    private String getCards = "[\n" +
+            "  {\n" +
+            "    \"Artist\": \"Matthew Robert Davies\",\n" +
+            "    \"Set_Name\": \"The First Chapter\",\n" +
+            "    \"Classifications\": \"Storyborn, Hero, Princess\",\n" +
+            "    \"Date_Added\": \"2023-08-18T12:00\",\n" +
+            "    \"Set_Num\": 1,\n" +
+            "    \"Color\": \"Amber\",\n" +
+            "    \"Gamemode\": \"Lorcana\",\n" +
+            "    \"Franchise\": \"\",\n" +
+            "    \"Image\": \"https://lorcana-api.com/images/ariel/on_human_legs/ariel-on_human_legs-large.png\",\n" +
+            "    \"Cost\": 4,\n" +
+            "    \"Inkable\": true,\n" +
+            "    \"Name\": \"Ariel - On Human Legs\",\n" +
+            "    \"Type\": \"Character\",\n" +
+            "    \"Lore\": 2,\n" +
+            "    \"Rarity\": \"Uncommon\",\n" +
+            "    \"Flavor_Text\": \"\\\"...\\\"\",\n" +
+            "    \"Unique_ID\": \"TFC-001\",\n" +
+            "    \"Card_Num\": 1,\n" +
+            "    \"Body_Text\": \"Voiceless - This character can't {e} to sing songs.\",\n" +
+            "    \"Willpower\": 4,\n" +
+            "    \"Card_Variants\": \"\",\n" +
+            "    \"Date_Modified\": \"2024-08-29 16:27:12.0\",\n" +
+            "    \"Strength\": 3,\n" +
+            "    \"Set_ID\": \"TFC\"\n" +
+            "  }]";
 
     @Test
     void testGetLorcanaSets(){
@@ -43,5 +71,14 @@ public class LorcanaServiceTest {
 
         assertNotNull(true);
         assertEquals(result, allSets);
+    }
+
+    @Test
+    void testGetAllCards(){
+        when(restTemplate.getForObject(anyString(), eq(String.class))).thenReturn(getCards);
+        String result = lorcanaService.getAllCards("1");
+
+        assertNotNull(true);
+        assertEquals(result, getCards);
     }
 }
