aa4bf0bbcd0efcaf54aa68616cc6465df076a7e3
andyzheng679 <andyzheng679@gmail.com>
2024-10-23 17:18:40 -0400

    lorcana controller getAllCards test

--- BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/ControllerTest/LorcanaControllerTest.java
+++ BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/ControllerTest/LorcanaControllerTest.java
@@ -4,6 +4,7 @@ package com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.ControllerTest;
 import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.controller.LorcanaController;
 import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.service.ImageDownloaderService;
 import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.service.LorcanaService;
+import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.tcgCard.Lorcana;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,11 +12,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -43,12 +46,23 @@ public class LorcanaControllerTest {
         when(lorcanaService.allSets(anyString())).thenReturn(mockMap);
         when(lorcanaService.getLorcanaSets()).thenReturn("set url");
 
-        mockMvc.perform(get("/lorcana/set")
-                .contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/lorcana/set").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\"Set 1\":\"1\",\"Set 2\":\"2\"}"));
+    }
 
+    @Test
+    public void testGetAllCards() throws Exception{
 
+        List<Lorcana> mockArrayList = new ArrayList<>();
+        mockArrayList.add(new Lorcana("name", "rarity", "imgURL"));
+
+        when(lorcanaService.getAllCards(anyString())).thenReturn("Json String of all cards");
+        when(lorcanaService.mapData("Json String of all cards")).thenReturn(mockArrayList);
+
+        mockMvc.perform(get("/lorcana/set/{setNum}", "1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"name\":\"name\",\"rarity\":\"rarity\",\"imgURL\":\"imgURL\"}]"));
     }
 
 
