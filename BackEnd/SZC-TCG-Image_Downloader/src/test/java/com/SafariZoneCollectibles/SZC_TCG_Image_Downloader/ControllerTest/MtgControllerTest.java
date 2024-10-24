42bd2d76bf2e8836e95e62e1ddb754ed1c370dc1
andyzheng679 <andyzheng679@gmail.com>
2024-10-23 23:21:21 -0400

    mtg controller getDataByCode test

--- BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/ControllerTest/MtgControllerTest.java
+++ BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/ControllerTest/MtgControllerTest.java
@@ -3,6 +3,7 @@ package com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.ControllerTest;
 import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.controller.MtgController;
 import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.service.ImageDownloaderService;
 import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.service.MtgService;
+import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.tcgCard.Mtg;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,7 +11,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.anyString;
@@ -46,4 +49,20 @@ public class MtgControllerTest {
                 .andExpect(content().json("{\"Set 1\":\"1\",\"Set 2\":\"2\"}"));
 
     }
+
+    @Test
+    public void testGetDataByCode() throws Exception{
+        ArrayList<Mtg> mockArrayList = new ArrayList<>();
+        mockArrayList.add(new Mtg("name", "rarity", "imgURL", "tcgplayerUrl"));
+
+        when(mtgService.getMtgCardSet(anyString())).thenReturn("Json String of all cards in set");
+        when(mtgService.mapData("Json String of all cards in set")).thenReturn(mockArrayList);
+
+        mockMvc.perform(get("/mtg/set/{code}", "1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"name\":\"name\",\"rarity\":\"rarity\",\"imgURL\":\"imgURL\", \"tcgplayerUrl\":\"tcgplayerUrl\"}]"));
+
+    }
+
+
 }
