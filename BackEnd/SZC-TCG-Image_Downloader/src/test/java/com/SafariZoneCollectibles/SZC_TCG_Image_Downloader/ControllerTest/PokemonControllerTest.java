879d06e0ba0bea184044682640eced4bd2375bb5
andyzheng679 <andyzheng679@gmail.com>
2024-10-24 13:42:57 -0400

    pokemon controller getAllPokemonSets test

---
+++ BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/ControllerTest/PokemonControllerTest.java
@@ -0,0 +1,49 @@
+package com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.ControllerTest;
+
+import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.controller.PokemonController;
+import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.service.ImageDownloaderService;
+import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.service.PokemonService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PokemonController.class)
+public class PokemonControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PokemonService pokemonService;
+
+    @MockBean
+    private ImageDownloaderService imageDownloaderService;
+
+    @Test
+    public void testGetAllPokemonSets() throws Exception{
+
+        Map<String, String> mockMap = new HashMap<>();
+        mockMap.put("Set 1", "1");
+        mockMap.put("Set 2", "2");
+
+        when(pokemonService.allSets(anyString())).thenReturn(mockMap);
+        when(pokemonService.getPokemonSets()).thenReturn("Json of all Sets");
+
+        mockMvc.perform(get("/pokemon/set").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"Set 1\":\"1\",\"Set 2\":\"2\"}"));
+    }
+
+}
