61ab54fb2452054c253fbb8c2216b11e614251e7
andyzheng679 <andyzheng679@gmail.com>
2024-10-24 23:33:31 -0400

    tried to test ImageDownloaderService, need to look into power mockito

--- BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/serviceTest/ImageDownloaderServiceTest.java
+++ BackEnd/SZC-TCG-Image_Downloader/src/test/java/com/SafariZoneCollectibles/SZC_TCG_Image_Downloader/serviceTest/ImageDownloaderServiceTest.java
@@ -1,10 +1,41 @@
 package com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.serviceTest;
 
+import com.SafariZoneCollectibles.SZC_TCG_Image_Downloader.service.ImageDownloaderService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
 
-@ExtendWith(MockitoExtension.class)
-public class ImageDownloaderServiceTest {
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
-    //may need to refactor code for testability. Current code will need to actually send a HTTP request. Come Back to this later.
-}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+//@ExtendWith(MockitoExtension.class)
+//public class ImageDownloaderServiceTest {
+//
+//    @InjectMocks
+//    private ImageDownloaderService imageDownloaderService;
+//
+//    @Test
+//    void testDownloadImage() throws Exception{
+//
+//        InputStream mockInputStream =  new ByteArrayInputStream("Image data".getBytes());
+//
+//        String cardName = "Name";
+//        String rarity = "Rarity";
+//        String imageUrl = "http://url.com";
+//
+//        ResponseEntity<InputStreamResource> response = imageDownloaderService.downloadImage(imageUrl, cardName, rarity);
+//
+//        assertNotNull(response);
+//        assertEquals("attachment; filename=\"Name_Rarity.png\"", response.getHeaders().getContentDisposition().toString());
+//        assertEquals("image/png", response.getHeaders().getContentType().toString());
+//    }
+//}
+
+// test takes 400 - 700ms to execute. Issue lies in making real HTTP requests, need to look into PowerMockito to solve this.
